73f450f01a8a1ce1fb3b859028d111fa9bc9031c
Eddykasp <maxkasperowski@gmail.com>
2015-06-28 11:44:20 +0800

    add gameState object constructor
    fix id of img tags

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+var game = new gameState();
+
 function playTurn(field) {
   cross(field);
 }
@@ -6,4 +8,25 @@ function cross(field) {
   field.src = "resources/cross.png";
   field.alt = "Cross";
   field.onclick = "";
+  game.fillField('X', Number(field.id.charAt(0)), Number(field.id.charAt(1)));
+  document.getElementById('debug').innerHTML = game.toString();
+}
+
+function gameState(){
+  this.state = [[' ', ' ', ' '],
+                [' ', ' ', ' '],
+                [' ', ' ', ' ']];
+  this.fillField = function (pl, row, col) {this.state[row][col] = pl;};
+  this.toString = function () {
+    var i,j,
+    str = "";
+    for(i = 0; i < 3; i++) {
+      str += "|";
+      for (j = 0; j < 3; j++) {
+        str += this.state[i][j] + "|";
+      }
+      str += "<br>";
+    }
+    return str;
+  }
 }
